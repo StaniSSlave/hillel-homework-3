ee434157cc493e26c781e8184678ab889476a6a5
Stanislave <stas24milioner@gmail.com>
2023-09-24 15:12:23 +0300

    Finish with calendar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Project start")
+# print("Project start")
 
 """
 У всіх завданнях використовувати обробку винятків
@@ -11,3 +11,40 @@ print("Project start")
 
 Залежно від введеної матем дії вивести результат
 """
+task_n = 1
+
+while task_n == 1 or task_n == 2 or task_n == 3:
+    try:
+            task_n = int(input("Select please task number, that you want to check: \n"
+                        "\t1. Користувач вводить із клавіатури номер дня тижня (1-7). Необхідно вивести на екран назви дня тижня.\n"
+                        "\t2. Користувач вводить два числа. Визначити, чи рівні ці числа, і, якщо ні, вивести їх на екран у порядку зростання\n"
+                        "\t3. Користувач вводить два числа та матем дію: + - * або /\n"
+                        "\t4. Stop checking tasks\n"
+                        "Enter choise here: "))
+
+            match task_n:
+                case 1:
+                    try:
+                        num_t1 = int(input("Enter please day number pew week to show it's name: "))
+
+                        match num_t1:
+                            case 1:
+                                print("This is Monday!")
+                            case 2:
+                                print("This is Tuesday!")
+                            case 3:
+                                print("This is Wednesday!")
+                            case 4:
+                                print("This is Thursday!")
+                            case 5:
+                                print("This is Friday!")
+                            case 6:
+                                print("This is Saturday!")
+                            case 7:
+                                print("This is Sunday!")
+                            case _:
+                                raise Exception("Please enter a valid day number!")
+                    except ValueError as e:
+                        print("Error: Please enter only integers!")
+                    except Exception as e:
+                        print(f"Error: {e}")
